13f48d364d0879c1757875d0ead3217619e7a94d
Kouji6309 <kouji6309@outlook.com>
2021-01-18 11:06:25 +0800

    rename

--- Client/Properties/AssemblyInfo.cs
+++ Client/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.19.307.1800")]
-[assembly: AssemblyFileVersion("2.19.307.1800")]
+[assembly: AssemblyVersion("2.21.118.1104")]
+[assembly: AssemblyFileVersion("2.21.118.1104")]
